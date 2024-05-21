5c8fa9d1a976524dd2d6ed8000ad969fae3ff209
unknown <you@example.com>
2024-05-22 03:41:15 +0800

    更新沒按鈕的情況

--- pyptt.py
+++ pyptt.py
@@ -56,13 +56,17 @@ def ptt_article(article_url, max_comment):
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(article_url)
 
-    load_all_button = driver.find_element(By.XPATH, '//div[text()="載入全部"]/parent::button')
-    load_all_button.click()
+    try:
+        load_all_button = driver.find_element(By.XPATH, '//div[text()="載入全部"]/parent::button')
+        load_all_button.click()
+    except:
+        print('留言過少')
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
 
-    article_title = soup.find('h1', 'title mt-2').text
+    article_title = soup.find('h1', 'title mt-2').text.strip()
+    article_title = re.sub('\s*Re:\s*', '', article_title)
     article_title = re.sub('\s*\[.*?\]\s*', '', article_title)
     print(article_title)
     
@@ -73,7 +77,7 @@ def ptt_article(article_url, max_comment):
             article_comment = article_comment.text
 
             if (not re.match('.*://.*',article_comment)):
-                print(article_comment)
+                #print(article_comment)
 
                 article_data.append({
                 "instruction": article_title,
@@ -89,19 +93,20 @@ def ptt_article(article_url, max_comment):
 
 def to_JSON():
     #url = 'https://www.pttweb.cc/hot/all/today'
-    #url = 'https://www.pttweb.cc/hot/not-news/today'
-    url = 'https://www.pttweb.cc/hot/comic/today'
+    url = 'https://www.pttweb.cc/hot/not-news/today'
+    # url = 'https://www.pttweb.cc/hot/comic/today'
 
     ptt_data = ptt_url(url,50,500)
 
-    with open('data/ptt_data.json', 'w', encoding='utf-8') as f:
+    with open('data/ptt_data1.json', 'w', encoding='utf-8') as f:
         json.dump(ptt_data, f, indent=4, ensure_ascii=False)
 
-#to_JSON()
+to_JSON()
 
 
 
-def test():
-    ptt_article('https://www.pttweb.cc/bbs/C_Chat/M.1716257370.A.D36', 500)
 
-test()
+# def test():
+#     ptt_article('https://www.pttweb.cc/bbs/C_Chat/M.1716247157.A.770', 5)
+
+# test()
