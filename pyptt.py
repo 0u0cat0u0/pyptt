844259dfae9468bb43b8a0fb4cc4d8ceba98fcd5
Rocky434 <necromancer434@gmail.com>
2024-05-22 00:14:17 +0800

    Add new function selenium

--- pyptt.py
+++ pyptt.py
@@ -82,3 +82,50 @@ def to_JSON():
 
 
 to_JSON()
+
+
+
+""" 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import re
+import time
+
+# 初始化ChromeDriver
+options = Options()
+options.headless = True  # 设置为True可以在后台运行Chrome
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+# 打开网页
+url = "https://www.pttweb.cc/bbs/C_Chat/M.1716253900.A.195"
+driver.get(url)
+
+# 循环点击“載入全部”按钮，直到按钮不存在
+while True:
+    try:
+        # 查找包含“載入全部”文本的div元素，然后获取其父级button元素
+        load_more_button = driver.find_element(By.XPATH, '//div[text()="載入全部"]/parent::button')
+        load_more_button.click()
+        time.sleep(2)  # 等待页面加载
+        break
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        # 按钮不存在时跳出循环
+        break
+
+# 获取加载后的页面内容
+soup = BeautifulSoup(driver.page_source, "html.parser")
+
+
+# 关闭WebDriver
+driver.quit()
+max_comment = 10
+# 输出加载后的页面内容
+article_title = soup.select_one('div' ,class_='yellow--text text--darken-2 e7-recommend-message').text
+print(article_title)
+ """
