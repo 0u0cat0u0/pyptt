ddbad004fa1a9adcaff36fa75fb9f36ded320950
unknown <you@example.com>
2024-05-22 02:09:56 +0800

    更新selenium

--- pyptt.py
+++ pyptt.py
@@ -1,7 +1,13 @@
 import requests
 import re
 import json
+import time
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 
@@ -23,7 +29,7 @@ def ptt_url(url, max_articles, max_comment):
             #print(article_title)
 
             if not (re.search('\s*\[發錢\]\s*',article_title) or re.search('\s*\[推投\]\s*',article_title) or re.search('.*?\(發錢\).*?',article_title) or re.search('.*?（發錢）.*?',article_title) or re.search('.*?直播單.*?',article_title)):
-                article_url = 'https://www.ptt.cc' + articles_found.find('a').get('href') + '.html'
+                article_url = 'https://www.pttweb.cc' + articles_found.find('a').get('href')
                 #print(article_url)
                 
                 article_data = ptt_article(article_url, max_comment)
@@ -39,20 +45,28 @@ def ptt_url(url, max_articles, max_comment):
 def ptt_article(article_url, max_comment):
     article_data = []
 
-    response = requests.get(article_url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    options = Options()
+    options.headless = True # 设置为True可以在后台运行Chrome
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(article_url)
 
-    article_title = soup.select_one('#main-content .article-metaline:nth-child(3) .article-meta-value').text
+    load_all_button = driver.find_element(By.XPATH, '//div[text()="載入全部"]/parent::button')
+    load_all_button.click()
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    driver.quit()
+
+    article_title = soup.find('h1', 'title mt-2').text
     article_title = re.sub('\s*\[.*?\]\s*', '', article_title)
-    #print(article_title)
+    print(article_title)
     
-    for article in soup.find_all('div', class_='push', limit = max_comment):
-        article_comment = article.find('span', class_='f3 push-content').text
-
-        article_comment = re.sub('^:? ', '', article_comment)
+    for article in soup.find_all('div', class_='yellow--text text--darken-2 e7-recommend-message', limit = max_comment):
+        article_comment = article.find('span').text
 
         if not re.match('.*://.*',article_comment):
-            #print(article_comment)
+            print(article_comment)
+
             article_data.append({
             "instruction": article_title,
             "input": "",
@@ -68,9 +82,9 @@ def to_JSON():
     #url = 'https://www.pttweb.cc/hot/not-news/today'
     url = 'https://www.pttweb.cc/hot/comic/today'
 
-    ptt_data = ptt_url(url,5,5)
+    ptt_data = ptt_url(url,3,3)
 
-    with open('ptt_data.json', 'w', encoding='utf-8') as f:
+    with open('data/ptt_data.json', 'w', encoding='utf-8') as f:
         json.dump(ptt_data, f, indent=4, ensure_ascii=False)
 
 
